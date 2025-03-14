47a5b4de3f4000840a9561551be1ac1887dff6b2
Dduong2195 <duong.dtt.2420@aptechlearning.edu.vn>
2025-03-14 22:17:51 +0700

    fix main-layout(staff)

--- src/main/java/com/cinema/starplex/ui/controllers/staff/MainLayoutController.java
+++ src/main/java/com/cinema/starplex/ui/controllers/staff/MainLayoutController.java
@@ -6,42 +6,39 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainLayoutController {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    // Xử lý sự kiện khi nhấn nút "Movie"
     @FXML
-    private void movieScene(ActionEvent event) throws IOException {
-        changeScene(event, "/com/cinema/starplex/staff/list-movie-layout.fxml");
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private void movieScene(ActionEvent event) {
+        loadPage("/com/cinema/starplex/staff/list-movie-layout.fxml");
     }
 
-    // Xử lý sự kiện khi nhấn nút "Room"
     @FXML
-    private void managementRoom(ActionEvent event) throws IOException {
-        changeScene(event, "/com/cinema/starplex/staff/.fxml");
+    private void managementRoom(ActionEvent event) {
+        loadPage("/com/cinema/starplex/staff/management-room.fxml");
     }
 
-    // Xử lý sự kiện khi nhấn nút "Seat"
     @FXML
-    private void managementSeat(ActionEvent event) throws IOException {
-        changeScene(event, "/com/cinema/starplex/staff/.fxml");
+    private void managementSeat(ActionEvent event) {
+        loadPage("/com/cinema/starplex/staff/management-seat.fxml");
     }
 
-    // Hàm chung để chuyển scene
-    private void changeScene(ActionEvent event, String fxmlPath) throws IOException {
-        root = FXMLLoader.load(getClass().getResource(fxmlPath));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    private void loadPage(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent page = loader.load();
+            mainBorderPane.setCenter(page);
+        } catch (IOException e) {
+            System.err.println("Unable to load FXML file: " + fxmlPath);
+            e.printStackTrace();
+        }
     }
 
     public void logout(ActionEvent actionEvent) {
